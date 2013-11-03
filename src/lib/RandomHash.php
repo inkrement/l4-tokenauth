2c4153511044a5998f28f1b295dae0c987e2f9fb
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2013-11-03 10:11:29 +0100

    token generator added

---
+++ src/lib/RandomHash.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace \Pichkrement\Tokenauth\Lib;
+
+use Illuminate\Support\Facades\Config as Config;
+
+class RandomHash {
+
+	private $length;
+	private $alpha;
+
+	public function __construct(){
+		//TODO $app->make('config')->get('tokenauth');
+	}
+
+	public function __construct(Config $config){
+		$this->legth = $config->get('length');
+		$this->alpha = $config->get('alpha');
+	}
+
+	public function __construct($legth, $alpha){
+		$this->legth = $legth;
+		$this->alpha = $alpha;
+	}
+
+
+	public function getRandomToken(){
+		$token = '';
+		$max = strlen($this->alpha) - 1;
+
+		for($i = 0; $i < $this->length; $i++)
+			$token .= $this->alpha[mt_rand(0, $max)];
+
+		return $token;
+	}
+
+}
+
+
+?>
